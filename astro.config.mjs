46e4d55ae0bb7b3c1a264abf4a9d8e0079b5a623
MengYYY369 <3209617055@qq.com>
2026-07-04 19:54:05 +0800

    Fix Pages domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://mengyyy369.github.io/",
+    site: "https://dayzmod.kdns.fr/",
     base: "/",
     trailingSlash: "always",
 
